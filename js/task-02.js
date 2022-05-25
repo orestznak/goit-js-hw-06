97166818676255babaa6a596cacf414f69090ec3
orestznak <51721977+orestznak@users.noreply.github.com>
2022-05-25 10:36:50 +0300

    task-02
    виконано task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const list = []
+
+for(let ingredient of ingredients){
+  const  item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+
+  list.push(item);
+
+}
+ingredientsList.append(...list);
+
+
