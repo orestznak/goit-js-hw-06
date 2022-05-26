70e4dbed224e0cf7127f46f7e9630aca9c30dd71
orestznak <orestznak@gmail.com>
2022-05-26 07:43:41 +0300

    Task-05
    виконав Task-05

---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+
+nameInput.addEventListener('input',(event) =>{
+    event.preventDefault();
+    
+    if(nameInput.value === ''){
+        nameOutput.textContent = "Anonymous";
+    } else {
+        nameOutput.textContent = event.currentTarget.value;
+    }
+})
