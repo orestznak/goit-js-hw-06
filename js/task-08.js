3ba0d863e8fd8165a89e800687e0c9d717ae5b64
orestznak <orestznak@gmail.com>
2022-05-28 06:49:21 +0300

    task-08
    виконав

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const loginForm = document.querySelector('form.login-form');
+console.log(loginForm);
+
+// const emailForm = loginForm.querySelector('.email')
+// console.log(emailForm);
+
+// const passwordForm = loginForm.querySelector('.password');
+// console.log(passwordForm);
+
+// const buttonSubmit = loginForm.querySelector('button')
+// console.log(buttonSubmit);
+
+const user = {};
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (loginForm.email.value === "" || loginForm.password.value === "") {
+        return alert("Please fill in all the fields!");
+    }
+
+    user.email = loginForm.email.value;
+    user.password = loginForm.password.value;
+
+    console.log(`User\nemail : ${user.email}\nuser password is : ${user.password}`);
+    loginForm.reset();
+});
 
