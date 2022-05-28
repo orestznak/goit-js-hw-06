b73d3b5a005527cfdfaf8ebc7523bf895504f3c1
orestznak <orestznak@gmail.com>
2022-05-28 07:00:16 +0300

    task-09
    виконав

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+
+const btnChangeColor = body.querySelector('.change-color');
+
+
+const nameColor = body.querySelector('.color');
+
+btnChangeColor.addEventListener('click', (event) =>{
+  event.preventDefault();
+
+  const changeColor = getRandomHexColor();
+  
+  body.style.background = changeColor;
+  nameColor.textContent = changeColor;
+
+});
