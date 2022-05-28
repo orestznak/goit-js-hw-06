da04ba8697f9e6c2fa69d28ce2fbc49e2bbccecf
orestznak <orestznak@gmail.com>
2022-05-28 06:14:58 +0300

    Task-07
    виконано

--- js/task-07.js
+++ js/task-07.js
@@ -1,22 +1,13 @@
 const rangeInput = document.querySelector('#font-size-control')
-const currentLength = rangeInput.value;
+
 
 const text = document.querySelector('#text')
 let outputText = text.textContent;
 console.log(outputText)
 
-rangeInput.addEventListener('input', (event) =>{
-    event.preventDefault();
-    //console.log(rangeInput.value)
-    const newText = outputText.slice(0,currentLength+1);
-    console.log(newText)
-
-
-
-
-   // text.textContent.length = rangeInput.value;
-
-
+rangeInput.addEventListener('input', () =>{
+    const currentInputValue = rangeInput.value;
+       text.style.fontSize = currentInputValue +'px';
 
 })
 
